4e140c3b2926f826cf019a38831269689a4285b2
AndresZamudioBdB <azamudi@bancodebogota.com.co>
2021-08-26 18:23:25 -0500

    small change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import {LandingComponent} from "./landing/landing.component";
 
 const routes: Routes = [
-  {
-    path: 'landing', component: LandingComponent
-  },
-  {
-    path: '**', component: LandingComponent
-  }
+  { path: 'landing', component: LandingComponent },
+  { path: '**', component: LandingComponent }
 ];
 
 @NgModule({
